6a5e23f873370627c4554740ad6898e9cc1b1af1
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-03-20 11:30:27 -0300

    feat: Criando uma classe Controller com a injeção dependência do Repository via Autowired

--- jwtsecurityon/src/main/java/jwtsecurityon/controller/UsuarioController.java
+++ jwtsecurityon/src/main/java/jwtsecurityon/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package jwtsecurityon.controller;
 import jwtsecurityon.dto.EntradaUsuarioDTO;
 import jwtsecurityon.entity.Usuario;
 import jwtsecurityon.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
-    private final UsuarioRepository usuarioRepository;
-    public UsuarioController(UsuarioRepository usuarioRepository) {
-        this.usuarioRepository = usuarioRepository;
-    }
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
 
     @PostMapping
     private EntradaUsuarioDTO salvar(@RequestBody EntradaUsuarioDTO dados){
